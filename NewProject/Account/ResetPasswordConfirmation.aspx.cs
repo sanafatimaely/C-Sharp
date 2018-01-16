84ad87f3d516d247891da89a092c827070b8f8ab
Sana Fatima <sf16aan@herts.ac.uk>
2018-01-16 21:26:17 +0100

    Add project files.

---
+++ NewProject/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace NewProject.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ NewProject/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewProject.Startup))]
+namespace NewProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
